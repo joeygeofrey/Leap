2edb9095c8996a0221a77558e985528e5527b0da
Joey <joeygeof.dev@gmail.com>
2023-04-07 15:34:22 -0400

    Update: featured jobs component

--- components/home/top/Topjobs.jsx
+++ components/home/top/Topjobs.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './topjobs.style'
+// import libs and components
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import styles from './topjobs.style';
+import { icons, SIZES } from '../../../constants';
+import TopJobCard from '../../common/cards/top/TopJobCard';
 
 const Topjobs = () => {
+  // initialize router and loading/error states
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
+
+  // render view for featured jobs
   return (
-    <View>
-      <Text>Topjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Featured Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>See all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          // display activity indicator if still loading
+          <ActivityIndicator size='large' colors={COLORS.primary} />  
+        ) : error ? (
+          // display error message if error occurs
+          <Text>Oops. Something's not right here.</Text>
+        ) : (
+          // else, display featured jobs
+          <FlatList 
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={({ item }) => (
+              <TopJobCard
+                item={item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
 
+// export TopJobs component as default
 export default Topjobs;
