185ca488608e410874d2a3f08896a41d35735ac3
Joey <joeygeof.dev@gmail.com>
2023-04-07 10:26:56 -0400

    Update: home component

---
+++ app/index.js
@@ -0,0 +1,15 @@
+// import view and text from react-native
+import { View, Text } from 'react-native';
+
+// define home component
+const Home = () => {
+    // render view text component
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+// export home component as default export
+export default Home;
