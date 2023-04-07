1781b6140fc9197c9173b6e77d833fb70d82ffd9
Joey <joeygeof.dev@gmail.com>
2023-04-07 15:49:21 -0400

    Add: Rapid API hook for jobs data

---
+++ hook/useFetch.js
@@ -0,0 +1,55 @@
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': RAPID_API_KEY,
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            // Make request
+            const response = await axios.request(options);
+            // Set data
+            setData(response.data.data);
+        } catch (error) {
+            if (error.response.status === 429) {
+            // Add 2 second delay before retrying the request
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            fetchData();
+        } else {
+            setError(error);
+            console.log(error);
+            alert('There is an error');
+        }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    };
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
