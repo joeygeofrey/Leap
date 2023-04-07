adf0eb0bc58ee92ab1b78ec4993cfe7e3108f3a2
Joey <joeygeof.dev@gmail.com>
2023-04-07 12:23:43 -0400

    Update: rename pop > top

---
+++ components/common/cards/top/TopJobCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './topjobcard.style'
+
+const TopJobCard = () => {
+  return (
+    <View>
+      <Text>TopJobCard</Text>
+    </View>
+  )
+}
+
+export default TopJobCard
---
+++ components/home/top/Topjobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './topjobs.style'
+
+const Topjobs = () => {
+  return (
+    <View>
+      <Text>Topjobs</Text>
+    </View>
+  )
+}
+
+export default Topjobs;
