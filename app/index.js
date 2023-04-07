189a3e39ef224b9bcb39541e868d843690e546cf
Joey <joeygeof.dev@gmail.com>
2023-04-07 12:43:49 -0400

    Update: main content and components

--- app/index.js
+++ app/index.js
@@ -1,13 +1,54 @@
-// import view and text from react-native
-import { View, Text } from 'react-native';
+// import libs and components
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { Topjobs, Popularjobs, Welcome, ScreenHeaderBtn, Nearbyjobs } from '../components';
+import { COLORS, icons, images, SIZES } from '../constants';
 
 // define home component
 const Home = () => {
-    // render view text component
+    // use router hook
+    const router = useRouter();
+    // render safeareaview with white bg
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            {/* stack screen */}
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightwhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => ( <ScreenHeaderBtn iconURL={icons.menu} dimenstion="60%" />),
+                    headerRight: () => ( <ScreenHeaderBtn iconURL={images.profile} dimenstion="100%" />),
+                    headerTitle: ''
+                }}
+            />
+            {/* scrollable view */}
+            <ScrollView showsVerticalScrollIndicator={false}>
+                {/* main content container */}
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }}
+                />
+                    {/* Welcome component */}
+                    <Welcome
+
+                    />
+
+                    {/* Topjobs component */}
+                    <Topjobs
+
+                    />
+
+                    {/* Nearbyjobs component */}
+                    <Nearbyjobs
+
+                    />
+
+
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
