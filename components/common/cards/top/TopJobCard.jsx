4bd5df595568f115c9016f4092e0e7aa24b7a59a
Joey <joeygeof.dev@gmail.com>
2023-04-07 19:59:47 -0400

    Update: add TopJobCard component

--- components/common/cards/top/TopJobCard.jsx
+++ components/common/cards/top/TopJobCard.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import libs and components
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import styles from './topjobcard.style';
+import { checkImageURL } from '../../../../utils';
 
-import styles from './topjobcard.style'
-
-const TopJobCard = () => {
+// define TopJobCard component
+const TopJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>TopJobCard</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      {/* render the logo image */}
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={checkImageURL(item.employer_logo)
+            ? { uri: item.employer_logo }
+            : require("../../../../assets/images/job.jpg")
+          }
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      {/* render company name and job info */}
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>{item.job_title}</Text>
+        <Text style={styles.location}>{item.job_city}, {item.job_state}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
-export default TopJobCard
+// export TopJobCard component as default
+export default TopJobCard;
