9367cf9f1bd35ded8ea878e01c4ef8811407a46a
Joey <joeygeof.dev@gmail.com>
2023-04-07 10:19:22 -0400

    Update: layout component

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+// import stack component from expo router
+import { Stack } from 'expo-router';
+
+// define layout component
+const Layout = () => {
+    // render stack component
+    return <Stack />
+}
+
+// export layout component as default export
+export default Layout;
