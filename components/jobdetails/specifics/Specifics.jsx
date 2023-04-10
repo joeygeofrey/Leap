0a104e798a390c384f559b15d578a4f1a34401fe
Joey <joeygeof.dev@gmail.com>
2023-04-10 13:23:15 -0400

    Update: Specifics component

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
 import styles from './specifics.style'
 
-const Specifics = () => {
+// functional component that renders title and list of points
+const Specifics = ({ title, points }) => {
   return (
-    <View>
-      <Text>Specifics</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}:</Text>
+
+      <View style={styles.pointsContainer}>
+        {points.map((item, index) => (
+          <View style={styles.pointWrapper} key={item + index}>
+            <View style={styles.pointDot} />
+            <Text style={styles.pointText}>{item}</Text>
+          </View>
+        ))}
+      </View>  
     </View>
   )
 }
 
+// export Specifics component as default
 export default Specifics
